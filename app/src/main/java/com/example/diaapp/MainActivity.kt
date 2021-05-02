ecd30055f8bde8d45a1367820b2e9e3a16e3369c
Mikhail Pankov <lbltnmrgnv@ya.ru>
2021-05-03 01:11:11 +0700

    Recipe and Calc activities

--- app/src/main/java/com/example/diaapp/MainActivity.kt
+++ app/src/main/java/com/example/diaapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.diaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val calc_btn : Button = findViewById(R.id.calc_btn)
+        calc_btn.setOnClickListener {
+            val intent = Intent(this@MainActivity, CalcActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        val recipes_bnt : Button = findViewById(R.id.recipes_btn)
+        recipes_bnt.setOnClickListener {
+            val intent = Intent(this@MainActivity, RecipesActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
